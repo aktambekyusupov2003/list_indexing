f787b3fb3d6b4d450c9af10ed46d72ba052a7373
aktam <aktam36911@gmail.com>
2023-03-10 19:20:08 +0500

    9

--- bin/list_index09.dart
+++ bin/list_index09.dart
@@ -6,4 +6,22 @@
     Returns:
         bool: return answer
 */
-void main() {}
+bool func(List list1) {
+  int a = 0;
+  int b = 1;
+  while (a < list1.length) {
+    int b = list1.length - 1;
+    int c = 1;
+    while (b > 0) {
+      if (list1[a] == list1[b]) {
+        True;
+        b += 1;
+        c += 1;
+        b = b - 1;
+      } else {
+        return false;
+      }
+      return False;
+    }
+  }
+}
